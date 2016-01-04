a4a6304e27c0b3fe8f1979a2dc8f3a1dcdc11336
amitabh <amitabh.p@scispl.com>
2016-01-04 08:54:18 +0530

    Added a test comment

--- src/main/java/com/goodvideotutorials/spring/config/MvcConfig.java
+++ src/main/java/com/goodvideotutorials/spring/config/MvcConfig.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+/**
+ * Comment of the class
+ * @author amitabh
+ *
+ */
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
